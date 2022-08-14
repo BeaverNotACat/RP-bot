42f80040bcfae8f28cb5c373ad7a845bbfa3f21a
C00prum29 <gavrish.mail@gmail.com>
2022-08-14 15:38:59 +0200

    Добавил вывод карты

---
+++ cogs/map.py
@@ -0,0 +1,27 @@
+from pydoc import describe
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Map(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @staticmethod
+    def __gain_embed() -> discord.Embed:
+
+        embed = discord.Embed(color=0xFFFFFF, title='Вывод карты местности:')
+        file = discord.File("images/map.png", filename="map.png")
+        embed.set_image(url="attachment://map.png")
+
+        return embed, file
+
+    @app_commands.command(name = 'map', description = 'Вывод карты района')
+    async def map(self, interaction: discord.Interaction) -> None:
+        embed_and_file = self.__gain_embed()
+        embed = embed_and_file[0]
+        file = embed_and_file[1]
+        await interaction.response.send_message(embed=embed, file=file)
+
+async def setup(bot):
+    await bot.add_cog(Map(bot))
