8118fe6a23f69081199a4f8456ad8399f9a9a265
beaver <beavernotacat@gmail.com>
2022-08-14 19:18:41 +1000

    Добавил больше проверок

--- classes/checkouts.py
+++ classes/checkouts.py
@@ -1,10 +1,39 @@
 import discord
+from database import DatabaseInterface
 
 
 class Checkouts:
     '''List of static utils methods, that checks user's unput'''
 
     @staticmethod
-    def check_admin_rights(interaction: discord.Interaction) -> None:
+    async def check_admin_rights(interaction: discord.Interaction) -> None:
         if interaction.guild.get_role(898307915348332614) not in interaction.user.roles:
-            raise discord.app_commands.AppCommandError('User access denied')
+            raise discord.app_commands.AppCommandError(
+                'Вы не имеете прав администратора')
+
+    @staticmethod
+    async def check_stat_name(stat: str) -> None:
+        if stat != 'мужество' or stat != 'мудрость' or stat != 'выдержка' or stat != 'справедливость':
+            raise discord.app_commands.AppCommandError(
+                'Неправильноt название характеристики')
+
+    @staticmethod
+    async def check_negative_heal(amount):
+        raise discord.app_commands.AppCommandError(
+            'Запрещено лечение на отрицательные значения')
+
+    @staticmethod
+    async def check_name(name: str, database: DatabaseInterface) -> None:
+        try:
+            await database.find_char_id(name)
+        except:
+            raise discord.app_commands.AppCommandError(
+                'Неправильное имя персонажа')
+
+    @staticmethod
+    async def check_inlfuence_money(name: str, database: DatabaseInterface) -> None:
+        try:
+            await database.read_money_amount(database.find_char_id(name))
+        except:
+            raise discord.app_commands.AppCommandError(
+                'Недостаточно средств для совершения оперции')
