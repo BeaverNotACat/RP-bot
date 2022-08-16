c7deef448158b034366bd3d5bc335e1039db0801
beaver <beavernotacat@gmail.com>
2022-08-17 03:05:08 +1000

    УБРАЛ СЛИП, ЗАХ ОН ОН ВООБЩЕ ТУТ НУЖЕН???

--- cogs/errors.py
+++ cogs/errors.py
@@ -2,8 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-import traceback
-
 
 class CommandErrorHandler(commands.Cog):
     def __init__(self, bot) -> None:
