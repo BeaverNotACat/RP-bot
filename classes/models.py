eb5a0b7ced8776c68a61f2f2cab5736a43b321dc
Beaver <beavernotacat@gmail.com>
2023-01-07 02:41:32 +1000

    :sparkles: Added ORM models

---
+++ classes/models.py
@@ -0,0 +1,101 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy_utils.types.choice import ChoiceType
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class User(Base):
+	__tablename__ = 'users'
+
+	TYPES = [('user', 'User'),
+	         ('admin', 'Admin')]
+
+	id = Column(Integer, primary_key=True)
+	type = Column(ChoiceType(TYPES))
+
+	characters = relationship("Character", back_populates="user", cascade="all, delete-orphan")
+
+	def __repr__(self):
+		return f"User(id={self.id!r}, fullname={self.type!r})"
+
+
+class Character(Base):
+	__tablename__ = 'characters'
+
+	TYPES = [('human', 'Human'),
+	         ('distortion', 'Distortion')]
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(100))
+	type = Column(ChoiceType(TYPES))
+	user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+	user = relationship(User, back_populates="characters")
+	skills = relationship('Skill', back_populates="character")
+	traits = relationship('Trait', back_populates="character")
+	weaknesses = relationship('Weakness', back_populates="character")
+	body_parts = relationship('BodyPart', back_populates="character")
+	items = relationship('Item', back_populates="character")
+
+	def __repr__(self):
+		return f"Character(id={self.id}, name={self.user}, type={self.type}, user={self.user})"
+
+
+class Skill(Base):
+	__tablename__ = 'skills'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(100))
+	description = Column(String)
+	profile_id = Column(ForeignKey(Character))
+
+	extra_info = relationship('ExtraInfo', back_populates="skills")
+
+
+class Trait(Base):
+	id = Column(Integer, primary_key=True)
+	text = Column(String(50))
+	character = Column(Integer, ForeignKey("characters.id"))
+
+
+class Weakness(Base):
+	id = Column(Integer, primary_key=True)
+	text = Column(String(50))
+	character = Column(Integer, ForeignKey("characters.id"))
+
+
+class BodyPart(Base):
+	id = Column(Integer, primary_key=True)
+	type = Column(String(50))
+	hp = Column(Integer)
+	max_hp = Column(Integer)
+	character = Column(Integer, ForeignKey("characters.id"))
+
+
+class Item(Base):
+	id = Column(Integer, primary_key=True)
+	type = Column(String(50))
+	amount = Column(Integer)
+	name = Column(String(100))
+	description = Column(String)
+	character = Column(Integer, ForeignKey("characters.id"))
+
+
+class Stats(Base):
+	character = Column(Integer, ForeignKey("characters.id"), primary_key=True)
+	fortitude = Column(Integer)
+	temperance = Column(Integer)
+	justice = Column(Integer)
+
+
+class ExtraInfo(Base):
+	__tablename__ = 'extra_info'
+
+	character = Column(Integer, ForeignKey("characters.id"), primary_key=True)
+	job = Column(String(100))
+	ideology = Column(String(50))
+	biography = Column(String)
+
+
+
+
